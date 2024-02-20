22a900d38529d9155106ca13fa9c34c84679d48f
Harrison Logan <65514149+hlogo37@users.noreply.github.com>
2024-02-20 16:54:21 -0500

    Update MinMaxCalculation.java
    added methods for min and max calculations in MinMaxCalculation class

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,1 +1,20 @@
-
+public class MinMaxCalculation {
+  public int findMin (int[] nums) {
+    int min = nums[0];
+    for(int i = 0; i < nums.length()-1; i++) {
+      if(nums[i] < min) {
+        nums[i] = min;
+      }
+    }
+    return min;
+  }
+  public int findMax (int[] nums) {
+    int max = nums[0];
+    for(int i = 0; i < nums.length()-1; i++) {
+      if(nums[i] > max) {
+        nums[i] = max;
+      }
+    }
+    return max;
+  }
+}
